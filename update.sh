63b23afb99fd259e842e0c908c52d77fbd145b80
ohir <ohir@fairbe.org>
2022-08-16 14:07:42 +0200

    prepare for pub.dev

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ if [ "`git branch --show-current`" == "main" -a -f .git/info/attributes ] ; then
 	echo "--- begin manual merge from $fro"
 	git merge -m "$umsg" --no-ff --no-commit $fro
 	git status -sb --porcelain
-	echo -e "--- next step: inspect above list and make git reset HEAD\n    or git add/rm to keep main branch clean"
+	echo "--- next step: inspect above list and make git reset HEAD\n    or git add/rm to keep main branch clean"
 else
 	echo "this script is useful only for the 'main' branch (configured)"
 fi
